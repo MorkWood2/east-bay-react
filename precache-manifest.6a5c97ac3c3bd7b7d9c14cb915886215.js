5e26a840d944f856a4f8906efed356d0ac4bd3bf
Mark Wild <mark.wild189@gmail.com>
2020-11-18 11:47:31 -0800

    Updates

---
+++ precache-manifest.6a5c97ac3c3bd7b7d9c14cb915886215.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d461cd8bfc3caff77e6ba8c4fd34a27e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c34cb78bf0d450cb97ee",
+    "url": "/static/css/main.b0d30001.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34cb78bf0d450cb97ee",
+    "url": "/static/js/main.6f7219b5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "99d6f7bde799f49179f899c610e19965",
+    "url": "/static/media/divider.99d6f7bd.svg"
+  },
+  {
+    "revision": "2279f1d2e64e4d5841a7bb42c681b582",
+    "url": "/static/media/doodle.2279f1d2.svg"
+  },
+  {
+    "revision": "2c1a5201a633e08df25fa5b94a70ce4b",
+    "url": "/static/media/footer.2c1a5201.svg"
+  },
+  {
+    "revision": "4012df899ad6599ccd85f7cbf718cd65",
+    "url": "/static/media/insigne - LeHavreRough-Primary.4012df89.otf"
+  },
+  {
+    "revision": "e41cf6e00a132477113d76a47f3ed532",
+    "url": "/static/media/logo.e41cf6e0.svg"
+  }
+]);
